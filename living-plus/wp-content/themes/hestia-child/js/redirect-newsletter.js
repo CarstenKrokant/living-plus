5122cf84a8036fd8212f0a3a8d17e0d3dd773bb0
CarstenKrokant <carsten@krokant.be>
2020-06-12 10:58:01 +0200

    Updated links

--- living-plus/wp-content/themes/hestia-child/js/redirect-newsletter.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener( 'wpcf7mailsent', function( event ) {
-    location = '/blijf-op-de-hoogte/bedankt/';
-}, false );
--- living-plus/wp-content/themes/hestia-child/js/redirect.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener( 'wpcf7mailsent', function( event ) {
-    location = '/contact/bedankt/';
-}, false );
